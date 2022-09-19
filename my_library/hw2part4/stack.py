f74e0cabc83ef5f013d03cc89254de8b3fa50788
sumaiya008 <112589818+sumaiya008@users.noreply.github.com>
2022-09-19 19:26:56 -0400

    writing again.  importing deque defining __init__

--- my_library/hw2part4/stack.py
+++ my_library/hw2part4/stack.py
@@ -2,40 +2,9 @@
 
 from collections import deque
 
-class Stack(object):
+class MyStack(object):
     def __init__(self):
         """
         Create an empty stack here.
         """
-        self.q = deque()
-
-# push element in stack
-     def push(self, x):
-        """
-        @param x, an int
-        return nothing
-        """
-        self.q.append( x )
-
-     def pop(self):
-        """
-        pop one item
-        return nothing
-        """
-        queueLen = len(self.q)
-        for i in range(queueLen-1):
-            x = self.q.popleft()
-            self.q.append(x)
-        self.q.popleft()  
-
-    def peek(self):
-        """
-        return top element
-        """
-        if self.q.is_empty():
-            raise IndexError('Stack have no element')
-            else:
-        for i in range(len(self.q)):
-            x = self.q.popleft()
-            self.q.append(x)
-        return x
+        self.queue = deque([])
