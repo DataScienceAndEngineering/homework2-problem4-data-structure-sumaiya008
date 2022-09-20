f9a8800cd9f8d9b59ac11479893545fd8210dbc5
sumaiya008 <112589818+sumaiya008@users.noreply.github.com>
2022-09-19 20:48:18 -0400

    remove item

--- my_library/hw2part4/queue.py
+++ my_library/hw2part4/queue.py
@@ -7,5 +7,13 @@ class MyQueue():
         self.items = []
 
     def enqueue(self, item):
-        #insert the item in the list
+        #insert the item to the front in the list
         self.items.insert(0,item)
+
+    def dequeue(self):
+        # removes and returns the last item in the list
+        if self.items.empty():
+            return False
+        else:
+            return self.items.pop()
+    
