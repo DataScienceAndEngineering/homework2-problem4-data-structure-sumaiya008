5c9883e979eeb00cd11b1e46d721ca976d1fe0ca
sumaiya008 <112589818+sumaiya008@users.noreply.github.com>
2022-09-19 23:38:57 -0400

    creating class node

---
+++ my_library/hw2part4/linked_list.py
@@ -0,0 +1,12 @@
+# Simple linked list implementation
+
+# creating class node.
+class Node(object):
+    # node carry data and a pointer which points to next data.
+    def __init__(self, data, next = None):
+        self.data = data;
+        self.next = next;
+
+#creating LinkedList class.
+class LinkedList:
+      
