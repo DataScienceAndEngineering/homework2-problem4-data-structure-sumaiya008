dfa16167ef3e7c57285fd50b5292572289dedef9
sumaiya008 <112589818+sumaiya008@users.noreply.github.com>
2022-09-20 17:24:38 -0400

    create empty bts class

--- my_library/hw2part4/trees.py
+++ my_library/hw2part4/trees.py
@@ -7,4 +7,10 @@ class Node:
         self.left = None
         self.right = None
         # to optimize memory and make search, insert, delete easier
-        
+
+#creating class BTS.
+class BinarySearchTree(object):
+
+    #creating an empty BST.
+    def __init__(self, root):
+        self.root = None
